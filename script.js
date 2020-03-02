c6ca42ef4ca58d22f03dc2c748ca747e111048c3
AnnaW1988 <anna.1.wroblewska@gmail.com>
2020-03-02 20:06:22 +0100

    Wersja finalna 1

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $(".mainMenu").prepend("<button class='hamburger' title='Pacnij by odkryć'>Menu</button>");
+  $("mainMenu ul").addClass("closedMenu");
+  $(".hamburger").on("click",function() {
+$(".mainMenu ul").slideToggle("closedMenu");
+});
+});
